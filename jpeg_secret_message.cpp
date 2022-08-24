277a506464093ab062ac17b0de552c9f8a74f38a
caleb1000 <caleb.steinmetz@yahoo.com>
2022-08-23 23:38:43 -0400

    cleaned up code

--- jpeg_secret_message.cpp
+++ jpeg_secret_message.cpp
@@ -41,27 +41,37 @@ unsigned char* cipher(unsigned char* data, int dataLength, uint32_t initialValue
         //print out new char
         //printf("%c",outputChar);
         printf("%02x",outputChar);
-
     }
+    printf("\n");
     return result;
 }
 
 
 int main (int argc, char *argv[]){
 
-    if(argc < 3){
-        std::cout<<"Missing Jpeg file as argument or Input string"<<std::endl;
+    if(argc < 3 || argc > 4){
+        std::cout<<"Too many arugments, missing Jpeg file or missing message string"<<std::endl;
         return -1;
     }
+
+    std::string filename;
+    if(argc == 4){
+        filename = argv[3];
+    }
+    else{
+        filename = "secret_message.jpeg";
+    }
+
     std::string message = argv[2];
-    std::ifstream jpeg;
-    jpeg.open(argv[1],std::ios::binary);
-    std::vector<unsigned char> result;
     int message_len = message.size();
+
     printf("Encrypted message in Hex: ");
     unsigned char* encrypted_message = cipher((unsigned char*)argv[2], message_len, 0x12345678);
-    printf("\n");
+
     unsigned char curr_char;
+    std::vector<unsigned char> result;
+    std::ifstream jpeg;
+    jpeg.open(argv[1],std::ios::binary);
     if(jpeg.is_open()){
         while(jpeg){
             curr_char = (unsigned char)jpeg.get();
@@ -70,17 +80,16 @@ int main (int argc, char *argv[]){
     }
     else{
         std::cout<<"Error opening jpeg"<<std::endl;
+        return -1;
     }
+
     result.pop_back();//remove 0xFF end char
-    int string_index = 0;
     int bit = 0; //current bit being encoded 0-7
-    uint8_t test =0;
+    int string_index = 0;
     while(string_index < message_len){
         result.push_back((1<<bit) & encrypted_message[string_index]);
-        test += ((1<<bit) & encrypted_message[string_index]);
         if(bit == 7){
             string_index++;
-            test = 0;
             bit = 0;
         }
         else{
@@ -88,9 +97,7 @@ int main (int argc, char *argv[]){
         }
     }
 
-    //cipher(encrypted_message, message_len, 0x12345678);
-
-    std::ofstream file("secret_message.jpeg", std::ios::out|std::ios::binary);
+    std::ofstream file(filename, std::ios::out|std::ios::binary);
     std::copy(result.cbegin(), result.cend(), std::ostream_iterator<unsigned char>(file));
     file.flush();
     file.close();
