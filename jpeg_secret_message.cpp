c842917e3b94b5be08098375dc74a19da0eb89e8
caleb1000 <caleb.steinmetz@yahoo.com>
2022-08-25 00:01:58 -0400

    cleaning up first cpp

--- jpeg_secret_message.cpp
+++ jpeg_secret_message.cpp
@@ -5,7 +5,30 @@
 #include <iterator>
 #include "cipher.h"
 
+
+std::vector<unsigned char> jpeg_to_vec(char* jpeg_name){
+    std::ifstream jpeg;
+    unsigned char curr_char;
+    std::vector<unsigned char> result;
+    std::string jpeg_name_string = jpeg_name;
+    jpeg.open(jpeg_name_string,std::ios::binary);
+    if(jpeg.is_open()){
+        while(jpeg){
+            curr_char = (unsigned char)jpeg.get();
+            result.push_back(curr_char);
+        }
+    }
+    else{
+        std::cout<<"Error opening jpeg"<<std::endl;
+    }
+    result.pop_back();//remove 0xFF aka end of file char
+    return result;
+}
+
+
 int main (int argc, char *argv[]){
+
+    //arg[1] = input jpeg, arg[2] = message, arg[3] = optional output filename
 
     if(argc < 3 || argc > 4){
         std::cout<<"Too many arugments, missing Jpeg file or missing message string"<<std::endl;
@@ -26,22 +49,12 @@ int main (int argc, char *argv[]){
     printf("Encrypted message in Hex: ");
     unsigned char* encrypted_message = cipher((unsigned char*)argv[2], message_len, 0x12345678, false);
 
-    unsigned char curr_char;
-    std::vector<unsigned char> result;
-    std::ifstream jpeg;
-    jpeg.open(argv[1],std::ios::binary);
-    if(jpeg.is_open()){
-        while(jpeg){
-            curr_char = (unsigned char)jpeg.get();
-            result.push_back(curr_char);
-        }
-    }
-    else{
-        std::cout<<"Error opening jpeg"<<std::endl;
+    std::vector<unsigned char> result; //vector to hold raw jpeg data
+    result = jpeg_to_vec(argv[1]);
+    if(result.empty()){
+        std::cout<<"Jpeg vector returned empty, exiting program"<<std::endl;
         return -1;
     }
-
-    result.pop_back();//remove 0xFF end char
     int bit = 0; //current bit being encoded 0-7
     int string_index = 0;
     while(string_index < message_len){
