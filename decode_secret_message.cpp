a17002fe2cb69714ac3cc2473930d49d2b1d0025
caleb1000 <caleb.steinmetz@yahoo.com>
2022-08-26 20:03:28 -0400

    making code cleaner

--- decode_secret_message.cpp
+++ decode_secret_message.cpp
@@ -5,6 +5,37 @@
 #include <iterator>
 #include "cipher.h"
 
+
+std::vector<uint8_t> parse_from_end(std::vector<unsigned char> result){
+	   int x=0;
+           std::vector<uint8_t> encrypted_message_vec;
+	   bool header_found = false;
+	   while(x<result.size()-3){
+	        if((result.at(x) == 0xFF) && (result.at(x+1) == 0xD9)){
+	            header_found = true;
+	            x=x+2;//when we identify the end header, we know the rest are going to be 8 bytes chuncks
+	            while(x<result.size()-1){
+	                uint8_t temp = 0;
+	                for(int y=0; y<8; y++){
+	                    temp += (uint8_t)result.at(x);
+	                    x++;
+	                }
+	                encrypted_message_vec.push_back(temp);
+	            }
+	        }
+	        else{
+	            x++;
+	        }
+	    }
+
+	    if(!header_found){
+	        std::cout<<"Secret message not found in jpeg"<<std::endl;
+	    }
+
+    return encrypted_message_vec;
+
+}
+
 std::vector<uint8_t> parse_from_chrom(std::vector<unsigned char> result){
     //we don't know full message size but cipher should still be able to work
     bool first_header_found = false;
@@ -31,7 +62,6 @@ std::vector<uint8_t> parse_from_chrom(std::vector<unsigned char> result){
                          bit++;
                      }
                 }
-                printf("\n");
                 break;
             }
             else{
@@ -54,15 +84,15 @@ int main (int argc, char *argv[]){
     if(argc == 3){
         std::string arg2(argv[2]);
             if(arg2 == "-i"){
-                std::cout<<"Parsing secret message within chromo of the jpeg"<<std::endl;
+                std::cout<<"Parsing secret message from chrominance values in target jpeg"<<std::endl;
                 inject_at_eof = false;
             }
             else{
-                std::cout<<"Parsing secret message at end of the jpeg"<<std::endl;
+                std::cout<<"Parsing secret message from end of target jpeg"<<std::endl;
             }
     }
     else{
-        std::cout<<"Parsing secret message at end of the jpeg"<<std::endl;
+        std::cout<<"Parsing secret message from end of target jpeg"<<std::endl;
     }
     std::ifstream jpeg;
     jpeg.open(argv[1],std::ios::binary);
@@ -78,71 +108,27 @@ int main (int argc, char *argv[]){
         std::cout<<"Error opening jpeg"<<std::endl;
         return -1;
     }
-    printf("jpeg size %ld\n",result.size());
-
+    unsigned char* encrypted_message;
+    std::vector<uint8_t> encrypted_message_vec;
     if(inject_at_eof){
-
-	   int x=0;
-	   bool header_found = false;
-	   std::vector<uint8_t> encrypted_message_vec;
-	   while(x<result.size()-3){
-	        if((result.at(x) == 0xFF) && (result.at(x+1) == 0xD9)){
-	            header_found = true;
-	            x=x+2;//when we identify the end header, we know the rest are going to be 8 bytes chuncks
-	            while(x<result.size()-1){
-	                uint8_t temp = 0;
-	                for(int y=0; y<8; y++){
-	                    temp += (uint8_t)result.at(x);
-	                    x++;
-	                }
-	                encrypted_message_vec.push_back(temp);
-	            }
-	        }
-	        else{
-	            x++;
-	        }
-	    }
-
-	    if(!header_found){
-	        std::cout<<"Secret message not found in jpeg"<<std::endl;
-	        return -1;
-	    }
-	    std::cout<<"Encrypted message in Hex: "<<std::endl;
-	    for(uint8_t i: encrypted_message_vec){
-	        printf("%02x",i);
-	    }
-	    printf("\n");
-	    unsigned char* encrypted_message;
-	    encrypted_message = (unsigned char*)malloc(encrypted_message_vec.size()*sizeof(unsigned char));
-	    unsigned char* temp = encrypted_message;
-	    for(int z=0; z<encrypted_message_vec.size(); z++){
-	        *temp = encrypted_message_vec.at(z);
-	        temp++;
-	    }
-	    std::cout<<"Decrypted message in ascii: "<<std::endl;
-	    cipher(encrypted_message, encrypted_message_vec.size(), 0x12345678, true);
-	    free(encrypted_message);
-	    jpeg.close();
-	    return 0;
-
+        encrypted_message_vec = parse_from_end(result);
     }
     else{
-        std::vector<uint8_t> encrypted_message_vec = parse_from_chrom(result);
-        std::cout<<"Encrypted message in Hex: ";
-        unsigned char* encrypted_message = (unsigned char*)malloc(encrypted_message_vec.size()*sizeof(unsigned char));
-        unsigned char* temp = encrypted_message;
-        for(int x =0; x< encrypted_message_vec.size(); x++){
-            printf("%02x",encrypted_message_vec.at(x));
-            *temp = (unsigned char)encrypted_message_vec.at(x);
-            temp++;
-        }
-        printf("\n");
-        std::cout<<"Decrypted message in ascii: "<<std::endl;
-        cipher(encrypted_message, encrypted_message_vec.size(), 0x12345678, true);
-        free(encrypted_message);
-        jpeg.close();
-        return 0;
+        encrypted_message_vec = parse_from_chrom(result);
     }
+    std::cout<<"Encrypted message in Hex: ";
+    encrypted_message = (unsigned char*)malloc(encrypted_message_vec.size()*sizeof(unsigned char));
+    unsigned char* temp = encrypted_message;
+    for(int x =0; x< encrypted_message_vec.size(); x++){
+        printf("%02x",encrypted_message_vec.at(x));
+        *temp = (unsigned char)encrypted_message_vec.at(x);
+        temp++;
+    }
+    printf("\n");
+    std::cout<<"Decrypted message in ascii: ";
+    cipher(encrypted_message, encrypted_message_vec.size(), 0x12345678, true);
+    free(encrypted_message);
+    jpeg.close();
 
 return 0;
 }
